b22fffc3fee2654d0dcb4367bb16bafadd31cbcc
kongmunist <andyking99@gmail.com>
2018-10-22 18:50:09 -0400

    Drawing rectangle around my face

--- faceMatch.py
+++ faceMatch.py
@@ -18,6 +18,17 @@ def getFace(file):
     rawFace = face_recognition.load_image_file(file)
     return face_recognition.face_encodings(rawFace)[0]
 
+
+# getWebcam()
+
+
+face = cv2.imread("myface.png")
+
+cv2.imshow('image',img)
+cv2.imshow('title', face)
+
+
+
 #me = getFace("myface.png")
 # notMe = getFace("notme.jpg")
 # identify = getFace("whoseface.JPG")
@@ -25,6 +36,14 @@ def getFace(file):
 # print(results)
 
 
+#
+# # top, right, bottom, left
+# fL = face_recognition.face_locations(face_recognition.load_image_file("myface.png"))
+# print(fL[0][0])
+# tL = (fL[0][3], fL[0][0])
+# bR = (fL[0][1], fL[0][2])
+# cv2.rectangle(face,tL,bR, (0, 0, 255), 2)
+# cv2.imshow("yah",face)
 
-face_locations = face_recognition.face_locations(face_recognition.load_image_file("myface.png"))
-print(face_locations)
+
+
