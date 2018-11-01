30ad35798c1a34ea6444e730f99393e3019525cd
kongmunist <andyking99@gmail.com>
2018-11-01 15:09:35 -0400

    Added generalized feature recognition

--- faceMatch.py
+++ faceMatch.py
@@ -8,62 +8,60 @@ from PIL import ImageDraw
 
 def getWebcam():
     webcam = cv2.VideoCapture(0)
-
+    #Frame coordinates go frame[y][x]
     while True:
         ret, frame = webcam.read()
-        lowFiFrame = copy.deepcopy(frame)
-        lowFiFrame = cv2.resize(lowFiFrame, (0,0), fy=.25, fx=.25)
+        lowFiFrame = cv2.resize(copy.deepcopy(frame), (0,0), fy=.25, fx=.25)
         locations = face_recognition.face_locations(lowFiFrame)
         feats = face_recognition.face_landmarks(lowFiFrame)
-        for spot in locations:
-            tL, bR = getCorners(spot,4)
-            cv2.rectangle(frame,tL,bR,(0,0,255),3)
-            # editFeat(frame, lowFiFrame, "left_eye", (0,255,255),4)
-            # editFeat(frame, lowFiFrame, "right_eye", (255,255,0),4)
-            # editFeat(frame, lowFiFrame, "right_eyebrow", (126,12,90),4)
-            editFeat(frame, lowFiFrame, feats, "left_eyebrow", (16,120,20),4)
 
-        if len(feats) > 1:
-            eyeList = []
-            for person in feats:
-                eyeList.append(person["chin"])
-            eyeRectList = []
-            for eye in eyeList:
-                eyeRectList.append(maxAndMin(eye))
-            print(eyeRectList)
-            # eyeSwap(frame, eyeRectList[0],eyeRectList[1])
-            try:
-                for i in range(eyeRectList[0][0]*4,eyeRectList[0][2]*4):
-                    for j in range(eyeRectList[0][1]*4, eyeRectList[0][3]*4):
-                        frame[j][i] = (0,0,0)
-            except:
-                pass
+        tagFaces(frame,locations)
+        featureSwap(frame, feats, "left_eye", "right_eye")
+
         cv2.imshow('frame', frame)
-
-
-
-
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     webcam.release()
     cv2.destroyAllWindows()
 
-def maxAndMin(eyeCoords):
+
+#Draws red box around faces it sees
+def tagFaces(frame, locations):
+    for spot in locations:
+        tL, bR = getCorners(spot, 4)
+        cv2.rectangle(frame, tL, bR, (0, 0, 255), 3)
+
+def featureSwap(frame, feats, feat1,feat2):
+    if len(feats) == 0:
+        return False
+    eyeList = []
+    for person in feats:
+        eyeList.append(person[feat1])
+        eyeList.append(person[feat2])
+    eyeRectList = []
+    for eye in eyeList:
+        eyeRectList.append(maxAndMin(eye))
+    for i in range(len(eyeList)//2):
+        eyeSwap(frame, eyeRectList[i], eyeRectList[i+1])
+
+def maxAndMin(featCoords):
+    adj = 5
     listX = []
     listY = []
-    for tup in eyeCoords:
+    for tup in featCoords:
         listX.append(tup[0])
         listY.append(tup[1])
-    return [min(listX),min(listY),max(listX),max(listY)]
+    return [min(listX)-adj,min(listY)-adj,max(listX)+adj,max(listY)+adj]
 
 def eyeSwap(frame, eye1, eye2):
     # Coords in the eyes go minx, miny, maxx, maxy
     try:
-        for i in range(eye1[2]-eye1[0]):
-            for j in range(eye1[3] - eye1[1]):
-                frame[eye2[0]+i][eye2[1]+j] = frame[eye1[0]+i][eye1[1]+j]
+        for x in range((eye1[2]-eye1[0]) * 4):
+            for y in range((eye1[3]-eye1[1]) * 4):
+                firstEye = copy.copy(frame[eye2[1]*4 + y][eye2[0]*4 + x])
+                frame[eye2[1]*4 + y][eye2[0]*4 + x] = frame[eye1[1]*4+y][eye1[0]*4+x]
+                frame[eye1[1]*4 + y][eye1[0]*4 + x] = firstEye
     except:
         pass
 
