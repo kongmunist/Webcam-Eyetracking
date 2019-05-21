b610f143dd501db7b184c1bf5c1a7599f11ae614
kongmunist <andyking99@gmail.com>
2019-05-20 19:11:34 -0500

    Trained x and y final models

--- andyCNN.py
+++ andyCNN.py
@@ -24,34 +24,60 @@ class ConvNet(torch.nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
 
-        f1 = 4
-        f2 = 16
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, f1, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(f1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
+        f2 = 4
         self.layer2 = nn.Sequential(
-            nn.Conv2d(f1, f2, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(f2),
+            nn.Conv2d(1, f2, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
+            nn.BatchNorm2d(f2),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc1 = nn.Linear(25 * 12 * f2, 400)
-        self.fc2 = nn.Linear(400, 60)
-        self.fc3 = nn.Linear(60, 1)
+        self.fc1 = nn.Linear(50 * 25 * f2, 200)
+        self.fc2 = nn.Linear(200, 20)
+        self.fc3 = nn.Linear(20, 1)
 
 
 
     def forward(self,x):
-        x = self.layer1(x);
-        x = self.layer2(x);
+        x = self.layer2(x)
         x = x.reshape(x.size(0), -1)
-        x = self.fc1(x);
-        x = self.fc2(x);
-        x = self.fc3(x);
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
 
         return x
 
+# class ConvNet(torch.nn.Module):
+#     def __init__(self):
+#         super(ConvNet, self).__init__()
+#
+#         f1 = 4
+#         f2 = 16
+#         self.layer2 = nn.Sequential(
+#             nn.Conv2d(f1, f2, kernel_size=5, stride=1, padding=2),
+#
+#             nn.ReLU(),
+#             nn.BatchNorm2d(f2),
+#             nn.MaxPool2d(kernel_size=2, stride=2))
+#         self.layer1 = nn.Sequential(
+#             nn.Conv2d(1, f1, kernel_size=5, stride=1, padding=2),
+#
+#             nn.ReLU(),
+#             nn.BatchNorm2d(f1),
+#             nn.MaxPool2d(kernel_size=2, stride=2))
+#         self.fc1 = nn.Linear(25 * 12 * f2, 400)
+#         self.fc2 = nn.Linear(400, 60)
+#         self.fc3 = nn.Linear(60, 1)
+#
+#
+#
+#     def forward(self,x):
+#         x = self.layer1(x)
+#         x = self.layer2(x)
+#         x = x.reshape(x.size(0), -1)
+#         x = self.fc1(x)
+#         x = self.fc2(x)
+#         x = self.fc3(x)
+#
+#         return x
 
 def dataLoad(path, want = 0):
     nameList = os.listdir(path)
@@ -93,74 +119,75 @@ def getError(model,testSet):
     return foeList
 
 
-num_epochs = 2
-
-model = ConvNet().to(device)
-# model.cuda()
-
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 trainingSet = dataLoad("eyes")
 test = dataLoad("testeyes")
 
-# trainingSet = [(x.to(device),y.to(device)) for (x,y) in trainingSet]
-# test = [(x.to(device),y.to(device)) for (x,y) in test]
-
-# print(trainingSet[-1])
-# print(test[-1])
 
 
-bestModel = model
-bestScore = 10000
-testscores = []
-trainscores = []
+num_epochs = 10
+bigTest = []
+bigTrain = []
 
-model.train()
-for epoch in range(num_epochs):
-    print(epoch)
-    np.random.shuffle(trainingSet)
+def trainModel():
+    model = ConvNet().to(device)
+    # model.cuda()
 
-    for i,(im, label) in enumerate(trainingSet):
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-        # print(im.device)
-        # print(label.device)
+    bestModel = model
+    bestScore = 10000
+    testscores = []
+    trainscores = []
 
+    model.train()
+    for epoch in range(num_epochs):
+        print(epoch)
+        np.random.shuffle(trainingSet)
 
-        output = model(im)
-        # if label.item()<.2:
-        #     print(i, output, label)
-        loss = criterion(output, label)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if (i+1) % 500 == 0:
-            testSc = evaluateModel(model,test)
-            trainSc = evaluateModel(model,trainingSet)
-            if testSc < bestScore:
-                bestModel = copy.deepcopy(model)
-                bestScore = testSc
-            testscores.append(testSc)
-            trainscores.append(trainSc)
-            print(trainSc)
-            print(testSc)
+        for i,(im, label) in enumerate(trainingSet):
 
 
+            output = model(im)
+            loss = criterion(output, label)
 
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                   .format(epoch+1, num_epochs, i+1, len(trainingSet), loss.item()))
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 1000 == 0:
+                # testSc = evaluateModel(model,test,sidelen=900)
+                testSc = evaluateModel(model,test)
+                # trainSc = evaluateModel(model,trainingSet,sidelen=900)
+                trainSc = evaluateModel(model,trainingSet)
+                if testSc < bestScore:
+                    bestModel = copy.deepcopy(model)
+                    bestScore = testSc
+                testscores.append(testSc)
+                trainscores.append(trainSc)
+
+                print(trainSc)
+                print(testSc)
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                       .format(epoch+1, num_epochs, i+1, len(trainingSet), loss.item()))
+
+    bigTest.append(testscores)
+    bigTrain.append(trainscores)
+
+    finalScore = evaluateModel(bestModel,test)
+    # finalScore = evaluateModel(bestModel,test,sidelen=900)
+    print(finalScore)
+
+    if finalScore < 150:
+        torch.save(bestModel.state_dict(), "xModels/" + str(int(finalScore))+".plt")
+
+    # plt.title(str(int(finalScore)))
+    # plt.plot(testscores)
+    # plt.plot(trainscores)
+
+for i in range(6):
+    trainModel()
 
 
-
-finalScore = evaluateModel(bestModel,test)
-print(finalScore)
-
-if finalScore < 100:
-    torch.save(bestModel.state_dict(), "xModels/" + str(int(finalScore))+".plt")
-
-plt.title(str(int(finalScore)))
-plt.plot(testscores)
-plt.plot(trainscores)
--- testEyes.py
+++ testEyes.py
@@ -8,6 +8,178 @@ import torch.nn as nn
 import torchvision
 import torch.functional as F
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+class eightdeep(torch.nn.Module):
+    def __init__(self):
+        super(eightdeep, self).__init__()
+
+        f2 = 8
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(1, f2, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.BatchNorm2d(f2),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc1 = nn.Linear(50 * 25 * f2, 200)
+        self.fc2 = nn.Linear(200, 20)
+        self.fc3 = nn.Linear(20, 1)
+
+
+
+    def forward(self,x):
+        x = self.layer2(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
+        return x
+
+class fourdeep(torch.nn.Module):
+    def __init__(self):
+        super(fourdeep, self).__init__()
+
+        f2 = 4
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(1, f2, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.BatchNorm2d(f2),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc1 = nn.Linear(50 * 25 * f2, 200)
+        self.fc2 = nn.Linear(200, 20)
+        self.fc3 = nn.Linear(20, 1)
+
+
+
+    def forward(self,x):
+        x = self.layer2(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
+        return x
+
+class venty(torch.nn.Module):
+    def __init__(self):
+        super(venty, self).__init__()
+
+        f2 = 8
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(1, f2, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(f2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc1 = nn.Linear(50 * 25 * f2, 200)
+        self.fc2 = nn.Linear(200, 10)
+        self.fc3 = nn.Linear(10, 1)
+
+
+
+    def forward(self,x):
+        x = self.layer2(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
+        return x
+
+class seven(torch.nn.Module):
+    def __init__(self):
+        super(seven, self).__init__()
+
+        f2 = 16
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(1, f2, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(f2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc1 = nn.Linear(50 * 25 * f2, 2000)
+        self.fc2 = nn.Linear(2000, 200)
+        self.fc3 = nn.Linear(200, 1)
+
+
+
+    def forward(self,x):
+        x = self.layer2(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
+        return x
+
+class eightfour(torch.nn.Module):
+    def __init__(self):
+        super(eightfour, self).__init__()
+
+        f1 = 8
+        f2 = 16
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, f1, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(f1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(f1, f2, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(f2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc1 = nn.Linear(25 * 12 * f2, 400)
+        self.fc2 = nn.Linear(400, 60)
+        self.fc3 = nn.Linear(60, 1)
+
+
+
+    def forward(self,x):
+        x = self.layer1(x);
+        x = self.layer2(x);
+        x = x.reshape(x.size(0), -1)
+        x = self.fc1(x);
+        x = self.fc2(x);
+        x = self.fc3(x);
+
+
+        return x
+
+
+class sixnine(torch.nn.Module):
+    def __init__(self):
+        super(sixnine, self).__init__()
+
+        f1 = 4
+        f2 = 16
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, f1, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(f1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(f1, f2, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(f2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc1 = nn.Linear(25 * 12 * f2, 400)
+        self.fc2 = nn.Linear(400, 60)
+        self.fc3 = nn.Linear(60, 10)
+        self.fc4 = nn.Linear(10, 1)
+
+
+
+    def forward(self,x):
+        x = self.layer1(x);
+        x = self.layer2(x);
+        x = x.reshape(x.size(0), -1)
+        x = self.fc1(x);
+        x = self.fc2(x);
+        x = self.fc3(x);
+        x = self.fc4(x);
+
+
+        return x
+
 class ConvNet(torch.nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
@@ -82,9 +254,101 @@ def getEye(model, times = 1,frameShrink = 0.15, coords = (0,0), counterStart = 0
             if cv.waitKey(1) & 0xFF == ord('q'):
                 break
 
+def dataLoad(path, want = 0):
+    nameList = os.listdir(path)
+
+    try:
+        nameList.remove(".DS_Store")
+    except:
+        pass
+    totalHolder = []
+    dims = [1440,900]
+
+    for name in nameList:
+        im = cv.cvtColor(cv.imread(path + "/" + name), cv.COLOR_BGR2GRAY)
+        top = max([max(x) for x in im])
+        totalHolder.append(((torch.tensor([[im]]).to(dtype=torch.float,device=device))/top,
+                            torch.tensor([[int((name.split("."))[want])/dims[want]]]).to(dtype=torch.float,device=device)))
+
+    # print(totalHolder)
+    return totalHolder
 
 
-mod = ConvNet()
-mod.load_state_dict(torch.load("xModels/96x.plt"))
-mod.eval()
-getEye(mod)
+def evaluateModel(model,testSet, sidelen = 1440):
+    # model.eval()
+    err = 0
+    for (im, label) in testSet:
+        output = model(im)
+        err += abs(output - label.item())
+    # model.train()
+
+    return (err/len(testSet)*sidelen)
+
+
+# X classifiers
+
+# sevsev = seven().to(device)
+# sevsev.load_state_dict(torch.load("xModels/77good.plt"))
+# sevsev.eval()
+#
+# sevn = ConvNet().to(device)
+# sevn.load_state_dict(torch.load("xModels/79good.plt"))
+# sevn.eval()
+#
+# eighfour = ConvNet().to(device)
+# eighfour.load_state_dict(torch.load("xModels/84good.plt"))
+# eighfour.eval()
+#
+# eighnine = ConvNet().to(device)
+# eighnine.load_state_dict(torch.load("xModels/89good.plt"))
+# eighnine.eval()
+#
+# eighfive = eightfour().to(device)
+# eighfive.load_state_dict(torch.load("xModels/85good.plt"))
+# eighfive.eval()
+#
+# se = fourdeep().to(device)
+# se.load_state_dict(torch.load("xModels/68.plt"))
+# se.eval()
+#
+# sn = fourdeep().to(device)
+# sn.load_state_dict(torch.load("xModels/69.plt"))
+# sn.eval()
+
+sixn = sixnine().to(device)
+sixn.load_state_dict(torch.load("xModels/69good.plt"))
+sixn.eval()
+
+sevent = venty().to(device)
+sevent.load_state_dict(torch.load("xModels/70test.plt"))
+sevent.eval()
+
+
+# Y classifiers
+fiv = eightdeep().to(device)
+fiv.load_state_dict(torch.load("yModels/54x1.plt"))
+fiv.eval()
+
+# sone = fourdeep().to(device)
+# sone.load_state_dict(torch.load("yModels/61.plt"))
+# sone.eval()
+#
+# stwo = fourdeep().to(device)
+# stwo.load_state_dict(torch.load("yModels/62.plt"))
+# stwo.eval()
+
+testy = dataLoad("testeyes",want=1)
+testx = dataLoad("testeyes")
+print(evaluateModel(sixn, test))
+
+trainx = dataLoad("eyes")
+trainy = dataLoad("eyes",want=1)
+
+
+def ensembleX(im): # 58 accuracy
+    modList = [sixn,sevent]
+    sumn = 0
+    for mod in modList:
+        sumn += mod(im).item()
+    return sumn / len(modList)
+
